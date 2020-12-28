763ff82ac596fd6cbf575aac3a9f168179679e1f
8329441266 <sadashivingale2810@gmail.com>
2020-12-28 01:33:08 -0800

    check glamber is loose or win per day

--- Utility.js
+++ Utility.js
@@ -2,27 +2,28 @@ class Utility{
     Simulation = () => {
         var stake = 100;
         var day = 1;
-        var winStake = 100;
-        var lostStake = 100;
         var r1,r2;
-        while(winStake<150 || lostStake>50){
-            var Random = Math.floor((Math.random()*10));
+        var winAmount;
+        var lostAmount;
+        while(day<=20){
+            var winStake = 100;
+            var lostStake = 100;
+            while(winStake<150 || lostStake>50){
+            var Random = Math.floor((Math.random()*10));           
             r1= Random%10;
             r2=r1%2;
-            if(r2 == 0){
+            if(r2 == 0){               
                 lostStake = lostStake-1;
             }
             else{
                 winStake = winStake+1;
             }
         }
-        if(winStake==150){
-            console.log("Gambler win 50% of stake");
-        }
-        else{
-            console.log("Gambler loose 50% of stake");
+        winAmount=winStake-stake;
+        lostAmount=stake-lostStake;
+        console.log("Win amount of day"+day+" is :"+winAmount+" and Lost amount of day"+day+" is :"+lostAmount);
+        day=day+1;
         }
     }
-
 }
 module.exports = new Utility();
